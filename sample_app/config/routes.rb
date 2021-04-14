b8106abec9f52b472e70ee78db25c76d5477edc2
mukku69 <programing0803@gmail.com>
2021-04-14 10:09:35 +0000

    [Finish]アプリケーションをテストしてみよう

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'todolists/new'
   get 'top' => 'homes#top'
-  post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
+  post 'todolists' => 'todolists#create'
   get 'todolists/:id' => 'todolists#show',as:'todolist'
   get 'todolists/:id/edit' => 'todolists#edit',as:'edit_todolist'
   patch 'todolists/:id' => 'todolists#update',as:'update_todolist'
